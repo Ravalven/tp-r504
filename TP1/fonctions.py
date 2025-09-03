926ca9f6a5fecf8ce03a3ff65e27f19b6641009f
Your Name <rventso@gmail.com>
2025-09-03 14:47:39 +0200

    Fonctions arrangées

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,4 +1,6 @@
 def puissance(a, b):
 	if type(a) is not int or type(b) is not int :
-		raise TypeError("Un entier svp")
+		raise TypeError("Un entier stp.")
+	if a == 0 and b < 0 :
+		raise ValueError("Impossible mathématiquement.")
 	return a ** b
