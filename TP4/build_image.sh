4ff5ce0781bacf2e9277619390f0196c1006345e
Ravalven <rventso@gmail.com>
2025-09-16 16:45:05 +0100

    v2.0

--- TP4/build_image.sh
+++ TP4/build_image.sh
@@ -1,1 +1,1 @@
-docker build -f Dockerfile1 -t im-tp4 .
+docker build -f Dockerfile2 -t im-tp4 .
--- TP4/run_app.sh
+++ TP4/run_app.sh
@@ -2,4 +2,5 @@ docker run -d \
   --name tp4-app \
   --network net-tp4 \
   -p 5000:5000 \
+  -v "$(pwd)"/srv/:/srv \
   im-tp4
